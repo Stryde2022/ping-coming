0f54bb0653573ae344312df43aa43a583d476c15
Stryde2022 <116350590+Stryde2022@users.noreply.github.com>
2025-01-31 13:04:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const form = document.querySelector("form");
 const warningText = document.querySelector(".warning");
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-form.addEventListener("submit", function validateInput(e)  {
+form.addEventListener("submit", function(e)  {
     if (!emailPattern.test(email.value)) {
         e.preventDefault();
         warningText.classList.remove("hide");
     }
 });
 
+email.addEventListener("input", function() {
+    console.log("error states are removed");
+    warningText.classList.add("hide");
+});
